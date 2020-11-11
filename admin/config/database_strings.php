ea383291dd87ae6f5fc0d3833c487c2c77838ca0
cedricfortuinaccommodatiehuur <70884826+cedricfortuinaccommodatiehuur@users.noreply.github.com>
2020-11-11 20:35:24 +0100

    update

--- admin/config/database_strings.php
+++ admin/config/database_strings.php
@@ -10,7 +10,7 @@ define('EXTERNAL_PASSWORD', 'Du2wfBtfqUq00FZgso8C');
 define('EXTERNAL_DATABASE', 'DATABASE_PLATENHUIS');
 
 //These strings are for the local database
-define ('LOCAL_HOST', 'localhost');
+define ('LOCAL_HOST', 'localhost:3307');
 define('LOCAL_USER', 'root');
 define('LOCAL_PASSWORD', 'root');
 define('LOCAL_DATABASE', 'hetplatenhuis');
--- admin/register_handler.php
+++ admin/register_handler.php
@@ -81,26 +81,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $user_role = trim($_POST["user_role"]);
     }
-
-//    // Validate password
-//    if (empty(trim($_POST["password"]))) {
-//        $password_err = "Please enter a password.";
-//    } elseif (strlen(trim($_POST["password"])) < 6) {
-//        $password_err = "Password must have at least 6 characters.";
-//    } else {
-//        $password = trim($_POST["password"]);
-//    }
-//
-//    // Validate confirm password
-//    if (empty(trim($_POST["confirm_password"]))) {
-//        $confirm_password_err = "Please confirm password.";
-//    } else {
-//        $confirm_password = trim($_POST["confirm_password"]);
-//        if (empty($password_err) && ($password != $confirm_password)) {
-//            $confirm_password_err = "Password did not match.";
-//        }
-//    }
-
+    
     // Check input errors before inserting in database
     if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($firstname_err) && empty($lastname_err) && empty($email_err) && empty($user_role_err)) {
 
